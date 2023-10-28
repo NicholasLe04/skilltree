f262fa8c3159a269c6ca51c910cf1d132bbfde33
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-10-27 22:57:00 -0700

    Create setup SQL queries

--- backend/setup.py
+++ backend/setup.py
@@ -5,6 +5,18 @@ def setup(connection):
         cursor.execute(
             '''
             CREATE TABLE IF NOT EXISTS user (
-                user_id SERIAL PRIMARY KEY,
+                username VARCHAR(60) NOT NULL UNIQUE PRIMARY KEY,
+                password VARCHAR(128) NOT NULL,
+                verified BOOLEAN NOT NULL,
+                description VARCHAR
+            );
+
+            CREATE TABLE IF NOT EXISTS skilltree (
+                skilltree_id SERIAL PRIMARY KEY,
+                username VARCHAR(60) REFERENCES user(username),
+                skill VARCHAR(128) NOT NULL,
+                description VARCHAR,
+                tree JSONB
+            );
             '''
         )
