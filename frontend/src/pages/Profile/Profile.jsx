a39d9e49aea9a6b4e9d5d04408475edd49555840
alex <alexnbui@gmail.com>
2023-10-28 13:59:38 -0700

    Add query for profile

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,15 +1,32 @@
 import Navbar from "../components/Navbar";
-import React from "react";
+import React, { useEffect } from "react";
 import TreeCard from "../components/TreeCard";
 import "./Profile.css"
+import DefaultPfp from "../../assets/images/default-pfp.png"
+import { useParams } from "react-router-dom";
 
 function Profile() {
+    const [user, setUser] = useState({})
+    const { username } = useParams();
 
-    // get profile based on username
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:6969/user/${username}`)
+            .then(res => {
+                setData(res.data)
+            })
+            .catch(err => {
+                setData({
+                    username: "User Not Found :(",
+                    verified: false,
+                    description: ""
+                })
+            })
+    }, [username])
+
     const verified = true;
     const name = "Oscar Crespo";
     const desc = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique vel eius qui fuga nesciunt esse consequatur labore in cumque doloribus. Eos architecto at eius voluptas aspernatur, consequuntur soluta ad ipsam.";
-    const pfpURL = "https://i.ytimg.com/vi/TC0YgcDbHHM/maxresdefault.jpg";
+    const pfpURL = DefaultPfp;
     const treeCards = [
         {
             title: "bruh",
@@ -55,7 +72,7 @@ function Profile() {
                 <div className="profile-hero">
                     <img src={pfpURL} className="profile-pfp"></img>
                     <div className="profile-header">
-                        <div className="profile-name">{name}{verified ? (<div className="profile-verif"><img src="https://static.vecteezy.com/system/resources/thumbnails/011/858/556/small/green-check-mark-icon-with-circle-tick-box-check-list-circle-frame-checkbox-symbol-sign-png.png" width={45} height={35}/></div>):null}</div>
+                        <div className="profile-name">{name}{verified ? (<div className="profile-verif"><img src="https://static.vecteezy.com/system/resources/thumbnails/011/858/556/small/green-check-mark-icon-with-circle-tick-box-check-list-circle-frame-checkbox-symbol-sign-png.png" width={45} height={35} /></div>) : null}</div>
                         <div className="profile-desc">{desc}</div>
                     </div>
                 </div>
--- frontend/src/pages/Tree/Tree.jsx
+++ frontend/src/pages/Tree/Tree.jsx
@@ -16,47 +16,47 @@ function Tree() {
     const [edges, setEdges] = useState([]);
 
     useEffect(() => {
-        setEdges([{ id: '1-2', source: '1', target: '2', type: 'straight', style: {stroke: '#000'} }, { id: '1-3', source: '1', target: '3', type: 'straight', style: {stroke: '#000'} } ]);
+        setEdges([{ id: '1-2', source: '1', target: '2', type: 'straight', style: { stroke: '#000' } }, { id: '1-3', source: '1', target: '3', type: 'straight', style: { stroke: '#000' } }]);
 
         setNodes([
-          {
-            id: '1',
-            data: { label: 'Hello' },
-            position: { x: 200, y: 300 },
-            style: { width: '50px', height: '50px', borderRadius: '25px', lineHeight: '25px', fontSize: '10px' },
-            targetPosition: 'bottom',
-            sourcePosition: 'top',
-            description: 'bruh'
-          },
-          {
-            id: '2',
-            data: { label: 'World' },
-            position: { x: 300, y: 200 },
-            style: { width: '50px', height: '50px', borderRadius: '25px', lineHeight: '25px', fontSize: '10px' },
-            targetPosition: 'bottom',
-            sourcePosition: 'top',
-            description: 'bruh2'
-          },
-          {
-            id: '3',
-            data: { label: 'World' },
-            position: { x: 100, y: 200 },
-            style: { width: '50px', height: '50px', borderRadius: '25px', lineHeight: '25px', fontSize: '10px' },
-            targetPosition: 'bottom',
-            sourcePosition: 'top',
-            description: 'bruh3'
-          },
+            {
+                id: '1',
+                data: { label: 'Hello' },
+                position: { x: 200, y: 300 },
+                style: { width: '50px', height: '50px', borderRadius: '25px', lineHeight: '25px', fontSize: '10px' },
+                targetPosition: 'bottom',
+                sourcePosition: 'top',
+                description: 'bruh'
+            },
+            {
+                id: '2',
+                data: { label: 'World' },
+                position: { x: 300, y: 200 },
+                style: { width: '50px', height: '50px', borderRadius: '25px', lineHeight: '25px', fontSize: '10px' },
+                targetPosition: 'bottom',
+                sourcePosition: 'top',
+                description: 'bruh2'
+            },
+            {
+                id: '3',
+                data: { label: 'World' },
+                position: { x: 100, y: 200 },
+                style: { width: '50px', height: '50px', borderRadius: '25px', lineHeight: '25px', fontSize: '10px' },
+                targetPosition: 'bottom',
+                sourcePosition: 'top',
+                description: 'bruh3'
+            },
         ])
     })
 
-    
+
 
 
     return (
         <>
             <Navbar />
-            <div className='main-content' style={{display:'flex', flexDirection:'column'}}>
-                <div className='main-content' style={{display:'flex'}}>
+            <div className='main-content' style={{ display: 'flex', flexDirection: 'column' }}>
+                <div className='main-content' style={{ display: 'flex' }}>
                     <div className='content'>
 
                         <ReactFlow nodes={nodes} edges={edges} fitView onNodeClick={(_, node) => setSelectedNode(node)} click>
@@ -66,8 +66,8 @@ function Tree() {
 
                     </div>
                     <div className='sidebar-container'>
-                        { 
-                            selectedNode && 
+                        {
+                            selectedNode &&
                             <>
                                 <h1>{selectedNode.data.label}</h1>
                                 <h3>Description</h3>
