b5c47090c8f831e0d43217fbf75db15c02dac345
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-10-28 02:04:42 -0700

    Fix global css

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
 import "./Login.css"
 
+
 function Login() {
+
+    const [login, setLogin] = useState(true)
+ 
     return (
         <>
-            <div className="container">
-                <div className="form-container" id="login-form">
+            <div className="login-container">
+                {login && <div className="form-container">
                     <h1>Login</h1>
                     <form>
                         <label htmlFor="username">Username</label>
@@ -13,21 +18,19 @@ function Login() {
                         <input type="password" id="password" name="password" required={true} />
                         <button type="submit">Login</button>
                     </form>
-                    <p>Don't have an account? <a href="#" id="signup-link">Sign up</a></p>
-                </div>
+                    <p>Don't have an account? <a id="signup-link" onClick={() => {setLogin(false)}}>Sign up</a></p>
+                </div>}
 
-                <div className="form-container" id="signup-form" style={{ display: "none" }} />
+                {!login && <div className="form-container" id="signup-form">
                 <h1>Sign Up</h1>
                 <form>
                     <label htmlFor="new-username">Username</label>
                     <input type="text" id="new-username" name="new-username" required={true} />
-                    <label htmlFor="new-email">Email</label>
-                    <input type="email" id="new-email" name="new-email" required={true} />
                     <label htmlFor="new-password">Password</label>
                     <input type="password" id="new-password" name="new-password" required={true} />
                     <button type="submit">Sign Up</button>
                 </form>
-                <p>Already have an account? <a href="#" id="login-link">Login</a></p>
+                <p>Already have an account? <a id="login-link" onClick={() => {setLogin(true)}}>Login</a></p></div>}
             </div>
         </>
     )
