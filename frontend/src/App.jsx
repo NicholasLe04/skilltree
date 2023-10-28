31b5c6a45303e043c1dc1ca80e8015390d0b17fb
Alex Bui <112739079+al3xbro@users.noreply.github.com>
2023-10-27 20:32:45 -0700

    Add Profile page (no logic yet) (#4)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,32 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
-import Create from './pages/Create';
 import Tree from './pages/Tree/Tree';
-import './App.css'
+import Create from './pages/Create';
+import Profile from './pages/Profile/Profile';
+import './App.css';
 
 function Root() {
-  return (
-    <>
-      <div className="Root">
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/tree/:treeID" element={<Tree />} />
-        </Routes>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="Root">
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path='/profile' element={<Profile />} />
+                    <Route path="/create" element={<Create />} />
+                    <Route path="/tree/:treeID" element={<Tree />} />
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 function App() {
-  return(
-    <BrowserRouter>
-        <Root/>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Root />
+        </BrowserRouter>
+    );
 }
 
 export default App
