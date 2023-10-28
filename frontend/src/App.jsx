f5635000a30f2d29891e01b4145f489e71851db1
alex <alexnbui@gmail.com>
2023-10-27 19:57:38 -0700

    Add Profile page (no logic yet)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,28 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
-import Create from './pages/Create';
-import './App.css'
+import Profile from './pages/Profile/Profile';
+import './App.css';
 
 function Root() {
-  return (
-    <>
-      <div className="Root">
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/create" element={<Create />} />
-        </Routes>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="Root">
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path='/profile' element={<Profile />} />
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 function App() {
-  return(
-    <BrowserRouter>
-        <Root/>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Root />
+        </BrowserRouter>
+    );
 }
 
 export default App
