65bc5f077a4f616ded67410dc2377268da352372
SphenOz <112739750+SphenOz@users.noreply.github.com>
2023-10-28 01:45:50 -0700

    Added routing to skilltree at upper left

--- frontend/src/pages/components/Navbar.jsx
+++ frontend/src/pages/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <div className="navbar">
-    <h1 style={{margin:"0"}}>skill<span style={{color:"#2ed142"}}>tree</span></h1>
+    <Link className='link' to="/Home"><h1 style={{margin:"0"}}>skill<span style={{color:"#2ed142"}}>tree</span></h1></Link>
         <div className="navbar-search">
         <input type="text" placeholder="Search" />
         <button>Search</button>
