51d3b7205df96764bf94f32a930fc13f70a86ebc
alex <alexnbui@gmail.com>
2023-10-27 20:17:27 -0700

    Fix routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Tree from './pages/Tree/Tree';
-import Create from './pages/Create';
+// import Create from './pages/Create';
 import Profile from './pages/Profile/Profile';
 import './App.css';
 
@@ -13,7 +13,7 @@ function Root() {
                     <Route path="/" exact element={<Home />} />
                     <Route path="/home" element={<Home />} />
                     <Route path='/profile' element={<Profile />} />
-                    <Route path="/create" element={<Create />} />
+                    {/* <Route path="/create" element={<Create />} /> */}
                     <Route path="/tree/:treeID" element={<Tree />} />
                 </Routes>
             </div>
