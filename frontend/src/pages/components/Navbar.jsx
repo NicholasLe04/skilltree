1c01817949593c297ad4d564008dcda283b47e3b
Joshua <112739750+SphenOz@users.noreply.github.com>
2023-10-28 02:12:31 -0700

    Navbar routing (#7)
    * Added routing to skilltree at upper left
    
    * Added nav to profile
    
    * Fix link color
    
    ---------
    
    Co-authored-by: NicholasLe04 <Nicholasl.le224@gmail.com>

--- frontend/src/pages/components/Navbar.jsx
+++ frontend/src/pages/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <div className="navbar">
-    <h1 style={{margin:"0"}}>skill<span style={{color:"#2ed142"}}>tree</span></h1>
+    <Link className='link' to="/Home"><h1 style={{margin:"0"}}>skill<span style={{color:"#2ed142"}}>tree</span></h1></Link>
         <div className="navbar-search">
         <input type="text" placeholder="Search" />
         <button>Search</button>
         </div>
         <ul className="navbar-list">
             <li className="navbar-item">Create</li>
-            <li className="navbar-item">Profile</li>
+            <Link className='link' to="/profile"><li className="navbar-item">Profile</li></Link>
         </ul>
     </div>
   );
