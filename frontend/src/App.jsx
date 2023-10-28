d92d5f463ee7624a5597e24e8ae457f7b08a8d69
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-10-27 18:06:27 -0700

    Create Skilltree View

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Create from './pages/Create';
+import Tree from './pages/Tree/Tree';
 import './App.css'
 
 function Root() {
@@ -11,6 +12,7 @@ function Root() {
           <Route path="/" exact element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/create" element={<Create />} />
+          <Route path="/tree/:treeID" element={<Tree />} />
         </Routes>
       </div>
     </>
