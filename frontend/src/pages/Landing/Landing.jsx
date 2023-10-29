56fb325cbf484b4fe9d83245a6c70db3050039ba
SphenOz <112739750+SphenOz@users.noreply.github.com>
2023-10-29 00:40:49 -0700

    CSS edits and Landing page improvements

--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -1,24 +1,16 @@
 import React, {createRef, useEffect} from 'react'
 import Navbar from '../components/Navbar';
-import logo from "../../assets/images/undraw_environment_iaus.png";
+import logo from "../../assets/images/undraw_environment_iaus.svg"
 import "./Landing.css"
 import { Link } from 'react-router-dom';
 
 function Landing() {
-    let wrapperRef = createRef();
-
-    useEffect(() => {
-        const wrapper = wrapperRef.current;
-        wrapper.classList.toggle('logo-has-loaded')
-    });
 
     return (
         <>
             <Navbar />
             <div className='landing'>
-                <div ref={wrapperRef} className='logo'>
-                    <img src={""} width={800} height={800-266}/>
-                </div>
+                <div className='logo'><img src={logo} width={700} height={800-266}></img></div>
                 <div className='welcome'>
                     <div className='wel'><h2>Welcome to skilltree</h2>
                         <Link className='link' to='/home'><button className='gstart'>Get Started!</button></Link></div>
