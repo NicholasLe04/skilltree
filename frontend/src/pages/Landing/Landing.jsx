4e0ea4cbf33106627c9e6dabb8d8c417f5496fe5
alex <alexnbui@gmail.com>
2023-10-28 22:23:23 -0700

    Finish display

--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import logo from "../../assets/images/undraw_environment_iaus.png";
-import "./landing.css";
+import "./Landing.css"
 import { Link } from 'react-router-dom';
 
 function Landing() {
@@ -10,11 +10,11 @@ function Landing() {
             <Navbar />
             <div className='landing'>
                 <div className='logo'>
-                    <img src={logo} width={800} height={800-266}/>
+                    <img src={logo} width={800} height={800 - 266} />
                 </div>
                 <div className='welcome'>
                     <div className='wel'><h2>Welcome to skilltree</h2>
-                    <Link className='link' to='/home'><button  className='gstart'>Get Started!</button></Link></div>
+                        <Link className='link' to='/home'><button className='gstart'>Get Started!</button></Link></div>
                 </div>
             </div>
         </>
